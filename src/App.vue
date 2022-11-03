4aba9f58ea6dfd18e773f32e231eb22897a98acd
MdAbdulKayumKhan <kayum261@gmail.com>
2022-11-03 18:20:50 +0600

    bootstrap and font-awesome install and configer in min.js file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>hello world</h2>
+  <button class="btn btn-success">hello</button>
+  <button class="btn btn-primary"> bangladesh</button>
+  <i class="fa fa-home text-success fa-4x"></i>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis error commodi aliquid nostrum corrupti amet illo rem animi, laborum inventore.</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ @import "./styles.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+/*
+ Font awesome 
+*/ 
+import "../node_modules/font-awesome/css/font-awesome.css"; 
+
+/* 
+    Bootstrap css, js
+*/ 
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+
 createApp(App).mount('#app')
